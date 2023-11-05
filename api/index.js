7a2071a297a4761fd9f955a2beef223bda4d24c9
sabrinabu <sabrinakhaled13@gmail.com>
2023-11-05 12:58:04 +0100

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
 const app=express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000,()=>{
   console.log("server ready")
